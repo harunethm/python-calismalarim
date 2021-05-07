467f198a82935ce19d4a2082fd7470b591a25356
Harun Ethem ÇALIŞ <48546060+harunethm@users.noreply.github.com>
2021-05-08 00:52:44 +0300

    Update CC#49.py

--- codingChallange/CC#49.py
+++ codingChallange/CC#49.py
@@ -1,4 +1,15 @@
 str1 = input("1. string : ")
 str2 = input("2. string : ")
 
+_str1 = set(str1)
+_str2 = set(str2)
+
+print(_str1)
+print(_str2)
+
+for i in _str1:
+  for j in _str2:
+    if i == j:
+      print(i + "-")
+
 #iki stringi karşılaştır aynı olan harfleri say
